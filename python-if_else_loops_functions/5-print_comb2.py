5e3ebbd4f74141b1fa9d18e10cfa2dd7e127b9b5
chutima puthachon <chutima.p8@gmail.com>
2024-09-03 16:01:30 +1000

    added print_comb

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num in range(0, 10):
-        print("0{}, ".format(num), end='')
-    elif num in range(10, 99):
-        print("{}, ".format(num), end='')
-print(99)
+    if num < 99:
+        print("{:02d}".format(num), end=', ')
+    elif num == 99:
+        print("{}".format(num), end='\n')
+
+# add new line and use prefix 0
