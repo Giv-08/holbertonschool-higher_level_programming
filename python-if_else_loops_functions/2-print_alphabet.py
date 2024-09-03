656d1013ea7db6de47d8d1795d03542c9814741c
chutima puthachon <chutima.p8@gmail.com>
2024-09-03 11:21:21 +1000

    added 2-print_alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import string
-def display_characters():
-    for char in string.ascii_lowercase:
-        print(char, end='')
-display_characters()
+# def display_characters():
+#     for char in string.ascii_lowercase:
+#         print(char, end='')
+# display_characters()
+print(string.ascii_lowercase, end="")
