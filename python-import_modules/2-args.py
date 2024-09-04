7ffff4940277163cf65958ad1311b77d4f341df1
chutima puthachon <chutima.p8@gmail.com>
2024-09-04 15:20:31 +1000

    added 2-argv.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import sys
+argv = sys.argv
+if __name__ == "__main__":
+    i = 0
+    for i in range(len(argv)):
+        if argv[i] == 0:
+            print("{} argument.".format(i), end="\n")
+        if argv[i] > 0:
+            print("{} arguments:".format(i), end="\n")
+            print("{}: {}".format(i, argv[i]), end="\n")
+        i = i + 1
