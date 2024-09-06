41bb6f94eaf0aa31a345d0f3a926b4b843eb9e09
chutima puthachon <chutima.p8@gmail.com>
2024-09-06 19:51:15 +1000

    added print_tebahpla

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,8 +4,8 @@ alphabets = [chr(a) for a in range(65, 91)]
 sorted_alphabets = sorted(alphabets, reverse=True)
 result = ""
 for i in sorted_alphabets:
-        if ord(i) % 2 == 0:
-            result += i.lower()
-        else:
-            result += i
-print("{}".format(result))
+    if ord(i) % 2 == 0:
+        result += i.lower()
+    else:
+        result += i
+print("{}".format(result), end="")
