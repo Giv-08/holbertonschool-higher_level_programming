a1995be67535a3f6573933e61271c9070d27e145
chutima puthachon <chutima.p8@gmail.com>
2024-09-09 13:06:13 +1000

    added new_in_list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for i in range(0, len(my_list)):
-        # my_list.reverse()
         new = my_list[::-1]
-        # if isinstance(my_list[i], int):
         print("{:d}".format(new[i], end=""))
-        # else:
-        #     return
-# while
-# no reverse
