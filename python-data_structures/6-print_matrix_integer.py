282d9aeb6ac0a24dfba36c131b9d4ea0953d1c1d
chutima puthachon <chutima.p8@gmail.com>
2024-09-09 14:41:31 +1000

    added add_tuple

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        # new = " ".join(map(str, matrix[i]))
-        # print("{:d}".format(new), end="\n")
         print(" ".join("{:d}".format(element) for element in i))
