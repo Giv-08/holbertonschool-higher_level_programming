93ff0a654b97e4f7cd584c350d322db516eb1a05
chutima puthachon <chutima.p8@gmail.com>
2024-09-09 13:34:29 +1000

    added no_c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,7 +3,6 @@ def no_c(my_string):
     new_string = ""
     for i in range(0, len(my_string)):
         if my_string[i] == "c" or my_string[i] == "C":
-            # new_string = "".join(i for i in my_string if i not in my_string)
-            new_string = my_string.translate({ord(i): None for i in 'cC'})
-            # new_string += my_string
+            # new_string = my_string.translate({ord(i): None for i in 'cC'})
+            new_string += my_string[:i] + my_string[i+1:]
             return(new_string)
