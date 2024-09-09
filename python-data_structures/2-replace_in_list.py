788ee6f7faf3c5bd94903f0fa4fcc1eecffd78c5
chutima puthachon <chutima.p8@gmail.com>
2024-09-09 11:05:54 +1000

    added 2-replace_in_list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,8 +2,9 @@
 def replace_in_list(my_list, idx, element):
     for i in range(0, len(my_list)):
         if i == idx:
-            my_list.remove(my_list[i])
-            my_list.insert(i, element)
+            # my_list.remove(my_list[i])
+            # my_list.insert(i, element)
+            my_list[i] = element
             return (my_list)
         elif i < 0 or i >= len(my_list):
             return my_list
