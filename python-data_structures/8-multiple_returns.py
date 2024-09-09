99224e30f89bb5137d387fb50e8d8f99908b5c6c
chutima puthachon <chutima.p8@gmail.com>
2024-09-09 14:54:54 +1000

    added 8-multiple_returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in range(0, len(sentence)):
-        if len(sentence) == 0:
-            return (0, None)
-        else:
-            return (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
