8ed983490281845462841cbe5095f723c6e0c271
chutima puthachon <chutima.p8@gmail.com>
2024-09-09 11:01:42 +1000

    added 2-replace_in_list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    # for i in range(0, len(my_list)):
-    #     if i == idx:
-    #         return ("{:d}".format(my_list[i]))
-    #     elif i < 0:
-    #         return None
-    #     elif i >= len(my_list):
-    #         return None
     for i, value in enumerate(my_list):
         if i == idx:
             return (value)
