8171dd306b778c5160983bef3adbe35765e7a0d3
chutima puthachon <chutima.p8@gmail.com>
2024-09-09 18:59:22 +1000

    added divisible_by_2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new = []
-    for i in range(0, len(my_list)):
+    for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            return True
+            new.append(True)
         else:
-            return False
+            new.append(False)
+    return(new)
