db3694af6fbeedbd01299b9d5897b20a4fcd3a4a
chutima puthachon <chutima.p8@gmail.com>
2024-09-10 12:22:04 +1000

    added common-element

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    # for i in set_1:
-    #     for j in set_2:
-    #         if i == j:
-    #             return (i)
-    #         else:
-    #             return []
-    result = [i for i in set_1 if i in set_2 ]
+    result = [i for i in set_1 if i in set_2]
     return result
