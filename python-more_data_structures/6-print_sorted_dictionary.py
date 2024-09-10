9e85f96e0b89fd897f1ceff1615510d40cb9a7f5
chutima puthachon <chutima.p8@gmail.com>
2024-09-10 14:05:49 +1000

    added print_sorted_dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     my_list = list(a_dictionary.keys())
     my_list.sort()
-    sorted_dict = {i : a_dictionary[i] for i in my_list}
+    sorted_dict = {i: a_dictionary[i] for i in my_list}
     for a, b in sorted_dict.items():
         print("{}: {}".format(a, b))
