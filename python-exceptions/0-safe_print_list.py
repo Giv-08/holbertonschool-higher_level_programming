fb9df8f73916c9510efb77bd7637c9c8d4c65469
chutima puthachon <chutima.p8@gmail.com>
2024-09-11 14:27:27 +1000

    added safe_print_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        length = 0
-        for i in range(x):
-            print(my_list[i], end="")
-            length += 1
-        print()
-        return length
-    except IndexError:
-            print("Error")
-            return length
     # try:
-    #     count = 0
+    #     length = 0
     #     for i in range(x):
     #         print(my_list[i], end="")
-    #         count += 1
+    #         length += 1
     #     print()
-    #     return count
+    #     return length
     # except IndexError:
-    #     print()
-    #     return count
+    #         print("Index(x) Error")
+    #         return length
+    try:
+        count = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
