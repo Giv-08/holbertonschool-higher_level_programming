8d5f0387a4255d0bd09e67f21960d7f761125675
chutima puthachon <chutima.p8@gmail.com>
2024-09-11 19:05:56 +1000

    added safe_print_division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = 0
+    try:
+        if a != 0 and b != 0:
+            result = a / b
+            return result
+        else:
+            return None
+    except ValueError:
+        print()
+    finally:
+        print("Inside result: {}".format(result or None))
