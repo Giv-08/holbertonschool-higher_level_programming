13a8f354c5591b39b9cae3796973adf11234a2a0
chutima puthachon <chutima.p8@gmail.com>
2024-09-12 10:28:28 +1000

    fixing

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,8 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             if isinstance(my_list_1[i], int) and isinstance(my_list_2[i], int):
-                raise TypeError
-            result = my_list_1[i] / my_list_2[i]
+                result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             result = 0
