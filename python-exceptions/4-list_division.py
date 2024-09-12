465e7260ecf45090c0dfeb5d42ad8e20c3e9d7a9
chutima puthachon <chutima.p8@gmail.com>
2024-09-12 10:34:30 +1000

    added raise

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,30 +1,12 @@
-#!/usr/bin/python3
-# def list_division(my_list_1, my_list_2, list_length):
-#     new = []
-#     for i in range(list_length):
-#         try:
-#             if isinstance(my_list_1[i], int) and isinstance(my_list_2[i], int):
-#                 result = my_list_1[i] / my_list_2[i]
-#         except TypeError:
-#             print("wrong type")
-#             result = 0
-#         except ZeroDivisionError:
-#             print("division by 0")
-#             result = 0
-#         except IndexError:
-#             print("out of range")
-#             result = 0
-#         finally:
-#             new.append(result)
-#     return new
+# !/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new = []
     for i in range(list_length):
         try:
-            # Ensure both elements are numeric (int or float)
-            if not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
+            if isinstance(my_list_1[i], int) and isinstance(my_list_2[i], int):
+                result = my_list_1[i] / my_list_2[i]
+            else:
                 raise TypeError
-            result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             result = 0
@@ -36,4 +18,4 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             new.append(result)
-    return new
+    return result
