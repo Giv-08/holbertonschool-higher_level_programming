8e2a1e35ffcd926fdef8be93b20aa9a68ff2afe2
chutima puthachon <chutima.p8@gmail.com>
2024-09-13 12:25:01 +1000

    added 4-print_square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module provides a function print_square(size)
+that print a square of '#'
+size has to be an integer or float
+TypeError is raised if size is not integer type.
+"""
+
+
+def print_square(size):
+    """
+    Prints square of #
+
+    Parameters:
+    size: dimension of thesquare
+
+    Returns:
+    size: length of the square
+
+    Raises:
+    TypeError: the size must be an integer and not less than 0
+    """
+    if isinstance(size, int):
+        for i in range(size):
+            for j in range(size):
+                print('#', end="")
+            print()
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    else:
+        raise TypeError("size must be an integer")
