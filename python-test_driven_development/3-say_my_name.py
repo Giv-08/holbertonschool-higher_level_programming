dad50d131d133212a86033986ad93321972ec69f
chutima puthachon <chutima.p8@gmail.com>
2024-09-13 10:47:33 +1000

    added 3-say_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -27,6 +27,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     if not last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
         print("My name is {}".format(first_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))
