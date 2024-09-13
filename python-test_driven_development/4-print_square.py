0e903107a39c736eda1707f97fd83c1776fbe584
chutima puthachon <chutima.p8@gmail.com>
2024-09-13 12:44:44 +1000

    added 4-print_square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -25,9 +25,7 @@ def print_square(size):
             for j in range(size):
                 print('#', end="")
             print()
-    elif size <= 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
-    elif isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-    else:
+    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
