fce7b5263d17f53bf4cc76c3353be8d8290ea328
chutima puthachon <chutima.p8@gmail.com>
2024-09-15 13:09:51 +1000

    added 2-matrix_divided

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -23,51 +23,30 @@ def matrix_divided(matrix, div):
     ZeroDivisionError: can't be divided by 0
     ValueError: If the rows of the matrix are not all the same size
     """
-    # if not isinstance(matrix, list):
-    #     raise TypeError
-    # ("matrix must be a matrix (list of lists) of integers/floats")
-    # if not all(isinstance(row, list) for row in matrix):
-    #     raise TypeError
-    # ("matrix must be a matrix (list of lists) of integers/floats")
-    # if not all(
-    #     isinstance(elem, (int, float))
-    #     for row in matrix
-    #     for elem in row
-    # ):
-    #     raise TypeError(
-    #         "matrix must be a matrix (list of lists) of integers/floats"
-    #     )
-    # length = len(matrix[0])
-    # if not all(len(row) == length for row in matrix):
-    #     raise TypeError("Each row of the matrix must have the same size")
-    # for row in matrix:
-    #     if len(row) != length:
-    #         raise TypeError("Each row of the matrix must have the same size")
-    # if not isinstance(div, (int, float)):
-    #     raise TypeError("div must be a number")
-    # if div == 0:
-    #     raise ZeroDivisionError("division by zero")
-
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
+    if not isinstance(matrix, list):
+        raise TypeError
+    ("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError
+    ("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(
+        isinstance(elem, (int, float))
+        for row in matrix
+        for elem in row
+    ):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
+    length = len(matrix[0])
+    if not all(len(row) == length for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     for row in matrix:
-        if not all(isinstance(el, (int, float)) for el in row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    length = set(len(row) for row in matrix)
-    if len(length) != 1:
-        raise ValueError("Each row of the matrix must have the same size")
-
+        if len(row) != length:
+            raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    row_length = len(matrix[0])
-    if any(len(row) != row_length for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
 
     new_matrix = []
     i = 0
