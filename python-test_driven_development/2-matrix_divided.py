ef315dbf4e32f6946aaea23a5621d489a447e751
chutima puthachon <chutima.p8@gmail.com>
2024-09-15 15:34:59 +1000

    added 3-say_my_name

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -24,36 +24,37 @@ def matrix_divided(matrix, div):
     ValueError: If the rows of the matrix are not all the same size
     """
     if not isinstance(matrix, list):
-        raise TypeError
-    ("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError
-    ("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if not all(
         isinstance(elem, (int, float))
         for row in matrix
         for elem in row
     ):
         raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
+            "matrix must be a matrix (list of lists) of integers/floats")
     length = len(matrix[0])
     if not all(len(row) == length for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+        raise TypeError(
+            "Each row of the matrix must have the same size")
     for row in matrix:
         if len(row) != length:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError(
+                "Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # new_matrix = []
-    # i = 0
-    # for i in matrix:
-    #     new_matrix.append([round(j / div, 2) for j in i])
-    # return new_matrix
-    return [[round(elem / div, 2) for elem in row] for row in matrix]
+    new_matrix = []
+    i = 0
+    for i in matrix:
+        new_matrix.append([round(j / div, 2) for j in i])
+    return new_matrix
+
 
 if __name__ == "__main__":
     import doctest
