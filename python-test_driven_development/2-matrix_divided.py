a1fa36885a84a4f8bb768fc1d0ee368a0c761d2e
chutima puthachon <chutima.p8@gmail.com>
2024-09-15 12:53:11 +1000

    added 2-matrix_divided

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -47,8 +47,9 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = []
-    i = 0
-    for i in matrix:
-        new_matrix.append([round(j / div, 2) for j in i])
-    return new_matrix
+    # new_matrix = []
+    # i = 0
+    # for i in matrix:
+    #     new_matrix.append([round(j / div, 2) for j in i])
+    # return new_matrix
+    return [[round(element / div, 2) for element in row] for row in matrix]
