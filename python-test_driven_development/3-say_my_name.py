77170c1b7c2ad634a8de355226d88d25e515ea71
chutima puthachon <chutima.p8@gmail.com>
2024-09-15 15:00:53 +1000

    added say_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,24 +7,41 @@ TypeError is raised if first_name or last_name are string type.
 """
 
 
+# def say_my_name(first_name, last_name=""):
+#     """
+#     Prints first string and second string
+
+#     Parameters:
+#     first_name: string
+#     last_name: string or default as empty string
+
+#     Returns:
+#     None
+
+#     Raises:
+#     TypeError: If first_name or last_name is not string
+#     """
+#     if not isinstance(first_name, str):
+#         raise TypeError("first_name must be a string")
+#     if not isinstance(last_name, str):
+#         raise TypeError("last_name must be a string")
+
+#     # print("My name is {} {}".format(first_name, last_name).strip())
+#     print(f"My name is {first_name} {last_name}".strip())
 def say_my_name(first_name, last_name=""):
     """
-    Prints first string and second string
+    Prints "My name is <first_name> <last_name>".
 
-    Parameters:
-    first_name: string
-    last_name: string or default as empty string
-
-    Returns:
-    None
+    Arguments:
+    first_name -- first name as a string
+    last_name -- last name as a string, defaults to an empty string
 
     Raises:
-    TypeError: If first_name or last_name is not string
+    TypeError -- if first_name or last_name is not a string
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    # print("My name is {} {}".format(first_name, last_name).strip())
     print(f"My name is {first_name} {last_name}".strip())
