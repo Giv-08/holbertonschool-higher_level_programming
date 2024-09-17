202ec6164712aeaaaf5224b4c8f4eeeaf3acf42b
chutima puthachon <chutima.p8@gmail.com>
2024-09-17 12:09:32 +1000

    added 0-rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module handles a rectangle area calculation.
+This module provides a function calculate_rectangle()
+that calculates the area of rectangle by using the length of one side
+"""
+
+
+class Rectangle:
