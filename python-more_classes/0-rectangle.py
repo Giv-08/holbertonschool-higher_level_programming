a31c006fd9e82e418926dd5fbf27af054aaa0e60
chutima puthachon <chutima.p8@gmail.com>
2024-09-17 12:12:17 +1000

    added 0-rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -6,3 +6,19 @@ that calculates the area of rectangle by using the length of one side
 
 
 class Rectangle:
+     """A class represents a rectangle.
+    Attributes
+    ----------
+    width : int
+        The width of the rectangle.
+    height : int
+        The height of the rectangle.
+
+    Methods
+    -------
+    __init__(width=0, height=0):
+        initializes the rectangle with a given side length
+    area():
+        Return the area of the rectangle
+
+    """
