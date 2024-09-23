9a4e9bf67efdfbc1820a920fe860b68ea33c87dd
chutima puthachon <chutima.p8@gmail.com>
2024-09-23 12:08:23 +1000

    added 1-my_list.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,7 +2,6 @@
 """
 This module provides a function that returns
 the list of available attributes and methods of an object
-TypeError is raised if size is not integer type.
 """
 
 
@@ -16,9 +15,5 @@ def lookup(obj):
     Returns:
     a list object
 
-    Raises:
-    TypeError: the matrix must be string or float type
-    ZeroDivisionError: can't be divided by 0
-    ValueError: If the rows of the matrix are not all the same size
     """
     return dir(obj)
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module provides a function that returns
+the list of sorted integers
+"""
+
+
+class MyList(list):
+    """
+    Prints the sorted list of integers
+
+    Parameters:
+    self: the list of integers
+
+    Returns:
+    a list of sorted integers
+    """
+
+    def print_sorted(self):
+        print(sorted(self))
