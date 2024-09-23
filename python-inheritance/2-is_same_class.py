9fbfd0a62aa80c17adc21d93bca32efbbd619637
chutima puthachon <chutima.p8@gmail.com>
2024-09-23 14:08:35 +1000

    2-added is_same_class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module provides a function that returns True
+if the object is exactly an instance of the specified class
+otherwise, False
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Custom list class that can print itself sorted.
+
+    Parameters:
+    self: the list of integers
+
+    Returns:
+    None
+    """
+    if (type(obj) == a_class):
+        return True
+    else:
+        return False
