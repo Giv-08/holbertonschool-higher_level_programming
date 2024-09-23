8471696e9659d85d1b4e95c6429dc53bcf8c3216
chutima puthachon <chutima.p8@gmail.com>
2024-09-23 11:37:48 +1000

    added 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
+"""
+This module provides a function that returns
+the list of available attributes and methods of an object
+TypeError is raised if size is not integer type.
+"""
+
 def lookup(obj):
+    """
+    Prints the list of available attributes and methods of an object
+
+    Parameters:
+    obj: a class passed in parameter
+
+    Returns:
+    a list object
+
+    Raises:
+    TypeError: the matrix must be string or float type
+    ZeroDivisionError: can't be divided by 0
+    ValueError: If the rows of the matrix are not all the same size
+    """
     return dir(obj)
