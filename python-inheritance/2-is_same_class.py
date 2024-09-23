500065693dff8f7a5f705ea8fd80a55a66067b3f
chutima puthachon <chutima.p8@gmail.com>
2024-09-23 14:09:46 +1000

    2-added is_same_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -16,7 +16,7 @@ def is_same_class(obj, a_class):
     Returns:
     None
     """
-    if (type(obj) == a_class):
+    if (type(obj) is a_class):
         return True
     else:
         return False
