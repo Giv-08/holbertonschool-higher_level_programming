3920f0cffc302816dec3b35631cc85273a81c484
chutima puthachon <chutima.p8@gmail.com>
2024-09-23 13:29:07 +1000

    added 1-main.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,15 +6,15 @@ the list of sorted integers
 
 
 class MyList(list):
-    """
-    Prints the sorted list of integers
-
-    Parameters:
-    self: the list of integers
-
-    Returns:
-    a list of sorted integers
-    """
 
     def print_sorted(self):
+        """
+        Prints the sorted list of integers
+
+        Parameters:
+        self: the list of integers
+
+        Returns:
+        a list of sorted integers
+        """
         print(sorted(self))
