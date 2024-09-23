f410a2a74b2c7d288cf084c467afae25db6714fd
chutima puthachon <chutima.p8@gmail.com>
2024-09-23 19:57:34 +1000

    added 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -20,7 +20,7 @@ def inherits_from(obj, a_class):
     Returns:
     None
     """
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
