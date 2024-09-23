71e450c3d1b05614fb9fa8a2c574ea320d02f9af
chutima puthachon <chutima.p8@gmail.com>
2024-09-23 19:35:20 +1000

    added 4-inherits_from.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides a class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+
+    Parameters:
+
+    Returns:
+    None
+    """
+    def area(self):
+        try:
+            pass
+        except BaseGeometry:
+            raise("area() is not implemented")
