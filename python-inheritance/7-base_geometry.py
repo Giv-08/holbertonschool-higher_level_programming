0eee9de52a154ecd8b847435c7ebac88805c5ac6
chutima puthachon <chutima.p8@gmail.com>
2024-09-24 09:29:12 +1000

    added all

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -38,7 +38,7 @@ class BaseGeometry:
         Returns:
         None
         """
-        if not isinstance(value, int) or type(value) is bool:
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
