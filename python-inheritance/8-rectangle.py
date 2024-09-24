796fd58a5c71d5a210160591b77c937b47ea0842
chutima puthachon <chutima.p8@gmail.com>
2024-09-24 10:48:21 +1000

    added 8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -52,16 +52,16 @@ class Rectangle(BaseGeometry):
         width: mustr be integer
         height: must be integer
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        # self.integer_validator("width", width)
+        # self.integer_validator("height", height)
 
         self.__width = width
         self.__height = height
-        # if not isinstance(self.__width, int):
-        #     raise super().integer_validator("width", width)
-        # if not isinstance(self.__height, int):
-        #     raise super().integer_validator("height", height)
-        # if self.__width < 0:
-        #     raise super().integer_validator("width", width)
-        # if self.__height < 0:
-        #     raise super().integer_validator("height", height)
+        if not isinstance(self.__width, int):
+            raise self.integer_validator("width", width)
+        if not isinstance(self.__height, int):
+            raise self.integer_validator("height", height)
+        if self.__width < 0:
+            raise self.integer_validator("width", width)
+        if self.__height < 0:
+            raise self.integer_validator("height", height)
