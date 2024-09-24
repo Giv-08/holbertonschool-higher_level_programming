e4b45c487b8aed369935cb581b7acccb8d4932d5
chutima puthachon <chutima.p8@gmail.com>
2024-09-24 12:34:40 +1000

    added 11-square.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -57,6 +57,7 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
+        super().__init__(size, size)
         # if not isinstance(self.__width, int):
         #     raise self.integer_validator("width", width)
         # if not isinstance(self.__height, int):
