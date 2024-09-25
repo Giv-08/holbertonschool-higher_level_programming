de82c650be11833c98c45f9eb22afc3029783538
chutima puthachon <chutima.p8@gmail.com>
2024-09-25 14:37:19 +1000

    added 100-my_int.py

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """
 This module provides a class called MyInt inherited from int
+the method MyInt behaves the opposite way during comparisons
 """
 
 class MyInt(int):
+    """
+    This is a class called MyInt inherited from int
+    """
     def MyInt(self):
+        """
+        the method MyInt behaves the opposite way during comparisons
+        "==" is equal "!="
+
+        Return: True is it's the opposite
+        otherwise, False
+        """
         if self == "==":
             return False
         elif self == "!=":
