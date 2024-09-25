0f27ba7e8c820dd06c44bcdfd80be147c3c71e0a
chutima puthachon <chutima.p8@gmail.com>
2024-09-25 14:34:12 +1000

    added 100-my_int.py

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module provides a class called MyInt inherited from int
+"""
+
+class MyInt(int):
+    def MyInt(self):
+        if self == "==":
+            return False
+        elif self == "!=":
+            return True
