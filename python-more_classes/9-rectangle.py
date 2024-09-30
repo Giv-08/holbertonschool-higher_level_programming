c85e14a9f79e9c419fd05da8e088f2a9db3634fb
chutima puthachon <chutima.p8@gmail.com>
2024-09-30 11:15:01 +1000

    added 0-read_file

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module handles a rectangle area calculation.
-This module provides a function calculate_rectangle()
+This module provides a function area()
 that calculates the area of rectangle by using the length of one side
 """
 
