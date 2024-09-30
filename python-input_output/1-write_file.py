db86fb889a7a79439ec198adbcb66312d4d56d05
chutima puthachon <chutima.p8@gmail.com>
2024-09-30 12:14:47 +1000

    added 2-append_write.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,6 +7,8 @@ def write_file(filename="", text=""):
     '''
     This function writes contents from the given file using "with"
     and open() and write()
+
+    Return: number of characters added
     '''
     with open(filename, 'w', encoding="utf-8") as op:
         op.write(text)
