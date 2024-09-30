c2b836cffe28dfcb3299ef9115463e40ae8f7a96
chutima puthachon <chutima.p8@gmail.com>
2024-09-30 20:04:59 +1000

    added 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module handles a function that read text in a given file
 """
-
+import json
 
 def read_file(filename=""):
     '''
@@ -9,13 +9,19 @@ def read_file(filename=""):
     and open()
     The last line must skip the new line
     '''
-    with open(filename, 'r', encoding="utf-8") as op:
-        lines = op.readlines()
-        length = len(lines)
-        if length == 1:
-            print(lines[0].strip())
-        else:
-            for i, line in enumerate(lines):
-                if i < length - 1:
-                    print(line, end="")
-            print(lines[-1].rstrip(), end="")
+    with open(filename, 'r') as file:
+        text = file.read()
+        for i, line in enumerate(text):
+            if i < len(text) - 1:
+                print(line, end="")
+        print(text[-1].rstrip(), end="")
+    # print(s)
+        # lines = file.readlines()
+        # length = len(lines)
+        # if length == 1:
+        #     print(lines[0].strip())
+        # else:
+        #     for i, line in enumerate(lines):
+        #         if i < length - 1:
+        #             print(line, end="")
+        #     print(lines[-1].rstrip(), end="")
