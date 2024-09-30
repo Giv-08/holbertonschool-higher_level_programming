6f12eda814d588233566d388ada28133cf405cdb
chutima puthachon <chutima.p8@gmail.com>
2024-09-30 20:08:23 +1000

    added 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,20 +10,19 @@ def read_file(filename=""):
     The last line must skip the new line
     '''
     with open(filename, 'r') as file:
-        text = file.read()
-        if len(text) == 1:
-            print(text[0].strip())
-        for i, line in enumerate(text):
-            if i < len(text) - 1:
-                print(line, end="")
-        print(text[-1].rstrip(), end="")
-    # print(s)
-        # lines = file.readlines()
-        # length = len(lines)
-        # if length == 1:
-        #     print(lines[0].strip())
-        # else:
-        #     for i, line in enumerate(lines):
-        #         if i < length - 1:
-        #             print(line, end="")
-        #     print(lines[-1].rstrip(), end="")
+        # text = file.read()
+        # if len(text) == 1:
+        #     print(text[0].strip())
+        # for i, line in enumerate(text):
+        #     if i < len(text) - 1:
+        #         print(line, end="")
+        # print(text[-1].rstrip(), end="")
+        lines = file.read()
+        length = len(lines)
+        if length == 1:
+            print(lines[0].strip())
+        else:
+            for i, line in enumerate(lines):
+                if i < length - 1:
+                    print(line, end="")
+            print(lines[-1].rstrip(), end="")
