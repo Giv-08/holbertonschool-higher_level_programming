0ed22355d01920ac2b26d472758a7db7d19db077
chutima puthachon <chutima.p8@gmail.com>
2024-09-30 12:15:36 +1000

    added 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""This module handles a function that appends a string at the end of a text file
-and returns the number of characters added
+"""This module handles a function that appends a string
+at the end of a text file and returns the number of
+characters added
 """
 
 
