290b14bde6542152dc68ba952fd0f9932b1281f0
chutima puthachon <chutima.p8@gmail.com>
2024-09-30 11:29:57 +1000

    added 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -27,13 +27,11 @@ def read_file(filename=""):
     with open(filename, 'r', encoding="utf-8") as op:
         lines = op.readlines()
         length = len(lines)
-        if length == 1:
-            # Single line, strip the newline at the end
-            print(lines[0].strip())
-        else:
-            # Print all but the last line as is
-            for i, line in enumerate(lines):
-                if i < length - 1:
-                    print(line, end="")
-            # Print the last line without an additional newline
-            print(lines[-1].rstrip(), end="")
+
+        for i, line in enumerate(lines):
+            # For all lines except the last, print them as is
+            if i < length - 1:
+                print(line, end="")
+            # For the last line, print it without the trailing newline
+            else:
+                print(line.rstrip(), end="")
