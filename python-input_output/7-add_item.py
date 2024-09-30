744ede4deb9967c2793bb0f62d7c795fbbf51c7e
chutima puthachon <chutima.p8@gmail.com>
2024-10-01 08:08:40 +1000

    added 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,6 +12,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv
 with open('add_item.json', 'a') as file:
     list = args[1:]
-    list.append()
+    list.extend(args[1:])
     save_to_json_file(list, 'add_item.json')
     load_from_json_file('add_item.json')
