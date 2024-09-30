1634c94904ac6701b31548516a344d896e416574
chutima puthachon <chutima.p8@gmail.com>
2024-09-30 11:00:59 +1000

    added 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 """This module handles a function that read text in a given file
 """
 
+
 def read_file(filename=""):
     '''
     This function prints contents from the given file using "with"
@@ -9,8 +10,9 @@ def read_file(filename=""):
     The last line must skip the new line
     '''
     with open(filename, 'r', encoding="utf-8") as op:
-        for line in op:
-            if line[:-1]:
-                print(line.rstrip(), end="")
-            else:
+        lines = op.readlines()
+        op = len(lines)
+        for i, line in enumerate(lines):
+            if i < op - 1:
                 print(line, end="")
+        print(lines[-1].rstrip(), end="")
