a985ac5d4faba6f0d3639c1d6346532b0b6aba4a
chutima puthachon <chutima.p8@gmail.com>
2024-09-30 11:28:56 +1000

    added 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,19 +3,37 @@
 """
 
 
+# def read_file(filename=""):
+#     '''
+#     This function prints contents from the given file using "with"
+#     and open()
+#     The last line must skip the new line
+#     '''
+#     with open(filename, 'r', encoding="utf-8") as op:
+#         lines = op.readlines()
+#         length = len(lines)
+#         if length == 1:
+#             print(lines[0].strip())
+#         else:
+#             for i, line in enumerate(lines):
+#                 if i < length - 1:
+#                     print(line, end="")
+#             print(lines[-1].rstrip(), end="")
 def read_file(filename=""):
     '''
-    This function prints contents from the given file using "with"
-    and open()
-    The last line must skip the new line
+    This function prints contents from the given file using "with" and open().
+    The last line must skip the new line.
     '''
     with open(filename, 'r', encoding="utf-8") as op:
         lines = op.readlines()
         length = len(lines)
         if length == 1:
+            # Single line, strip the newline at the end
             print(lines[0].strip())
         else:
+            # Print all but the last line as is
             for i, line in enumerate(lines):
                 if i < length - 1:
                     print(line, end="")
+            # Print the last line without an additional newline
             print(lines[-1].rstrip(), end="")
