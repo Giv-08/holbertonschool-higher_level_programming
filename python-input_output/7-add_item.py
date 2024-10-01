f2ea2d4a37996117bc85d68fc0b0feed5fdbe3da
chutima puthachon <chutima.p8@gmail.com>
2024-10-01 19:29:07 +1000

    added 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,18 +3,16 @@
 arguments to a Python list, and then save them to a file:
 """
 import sys
-import os
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-# file = 'add_item.json'
-# with open('add_item.json', 'a') as file:
-if os.path.exists('add_item.json'):
-    lst = load_from_json_file('add_item.json')
-else:
+file = 'add_item.json'
+
+try:
+    lst = load_from_json_file(file)
+except FileNotFoundError:
     lst = []
 lst.extend(sys.argv[1:])
-# load_from_json_file('add_item.json')
 save_to_json_file(lst, 'add_item.json')
