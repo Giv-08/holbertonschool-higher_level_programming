e1ff9e7b43e0fea9d318dfef570fd3488ac12334
chutima puthachon <chutima.p8@gmail.com>
2024-10-01 19:22:52 +1000

    added 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,8 +9,9 @@ import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+file = 'add_item.json'
 # with open('add_item.json', 'a') as file:
-if os.path.exists('add_item.json'):
+if os.path.exists(file):
     lst = load_from_json_file('add_item.json')
 else:
     lst = []
