183f57b43b4e85d0830255eac5f1cb73f9607e3a
chutima puthachon <chutima.p8@gmail.com>
2024-10-01 19:31:43 +1000

    added 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,17 +2,32 @@
 """This module writes a script adds all
 arguments to a Python list, and then save them to a file:
 """
+# import sys
+
+
+# save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+# load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+# file = 'add_item.json'
+
+# try:
+#     lst = load_from_json_file(file)
+# except FileNotFoundError:
+#     lst = []
+# lst.extend(sys.argv[1:])
+# save_to_json_file(lst, 'add_item.json')
 import sys
+import os
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file = 'add_item.json'
+# file = 'add_item.json'
 
-try:
-    lst = load_from_json_file(file)
-except FileNotFoundError:
+if os.path.exists('add_item.json'):
+    lst = load_from_json_file('add_item.json')
+else:
     lst = []
 lst.extend(sys.argv[1:])
 save_to_json_file(lst, 'add_item.json')
