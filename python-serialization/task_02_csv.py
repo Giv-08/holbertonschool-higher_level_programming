9fb888363bd6d60732283ab5abb099fc1a135e50
chutima puthachon <chutima.p8@gmail.com>
2024-10-02 21:55:31 +1000

    added task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -19,31 +19,8 @@ def convert_csv_to_json(csv_file):
             print("Conversion successful!")
             return True
         else:
-            # print("No CSV file provided.")
             return False
     except FileNotFoundError:
-        # print(f"File not found: {csv_file}")
         return False
     except Exception as e:
-        # print(f"An error occured: {e}")
         return False
-# def convert_csv_to_json(csv_file):
-#     try:
-#         if csv_file:
-#             with open(csv_file, 'r') as f:
-#                 my_dict = DictReader(f)
-#                 list_dict = list(my_dict)
-
-#             with open('data.json', 'w') as file:
-#                 json.dump(list_dict, file, indent=4)
-#             print("Conversion successful!")
-#             return True
-#         else:
-#             print("No CSV file provided.")
-#             return False
-#     except FileNotFoundError:
-#         print(f"File not found: {csv_file}")
-#         return False
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-#         return False
