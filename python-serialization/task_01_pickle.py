b4c26a7e9654ceaf9bbb76b1e4cfb4d431bcf59b
chutima puthachon <chutima.p8@gmail.com>
2024-10-02 20:16:16 +1000

    added task_01_pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module handles class called CustomObject
+"""
+import json
+# import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.__name = name
+        self.__age = age
+        self.__is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.__name}")
+        print(f"Age: {self.__age}")
+        print(f"Is Student: {self.__is_student}")
+
+    def serialize(self, filename):
+        data = {
+            'name': self.__name,
+            'age': self.__age,
+            'is_student': self.__is_student
+        }
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            return cls(data['name'], data['age'], data['is_student'])
