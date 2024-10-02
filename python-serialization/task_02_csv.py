15a8a2dd8c3636e28329d12c831d1e4e405e94b3
chutima puthachon <chutima.p8@gmail.com>
2024-10-02 21:49:50 +1000

    added task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,44 +7,43 @@ from csv import DictReader
 import json
 
 
-# def convert_csv_to_json(cvs_file):
-#     try:
-#         if cvs_file:
-#             with open('csv_file', 'r') as f:
-#                 my_dict = DictReader(f)
-#                 list_dict = list(my_dict)
-
-#             with open('data.json', 'w') as file:
-#                 json.dump(list_dict, file)
-#             print("Conversion successful!")
-#             return True
-#         else:
-#             print("No CSV file provided.")
-#             False
-#     except FileNotFoundError:
-#         print(f"File not found: {csv_file}")
-#         return False
-#     except Exception as e:
-#         print(f"An error occured: {e}")
-#         return False
-def convert_csv_to_json(csv_file):
+def convert_csv_to_json(cvs_file):
     try:
-        if csv_file:
-            print(f"Attempting to open CSV file: {csv_file}")
-            with open(csv_file, 'r') as f:
+        if cvs_file:
+            with open('csv_file', 'r') as f:
                 my_dict = DictReader(f)
                 list_dict = list(my_dict)
 
             with open('data.json', 'w') as file:
-                json.dump(list_dict, file, indent=4)
+                json.dump(list_dict, file)
             print("Conversion successful!")
             return True
         else:
-            print("No CSV file provided.")
-            return False
+            # print("No CSV file provided.")
+            False
     except FileNotFoundError:
-        print(f"File not found: {csv_file}")
+        # print(f"File not found: {csv_file}")
         return False
     except Exception as e:
-        print(f"An error occurred: {e}")
+        # print(f"An error occured: {e}")
         return False
+# def convert_csv_to_json(csv_file):
+#     try:
+#         if csv_file:
+#             with open(csv_file, 'r') as f:
+#                 my_dict = DictReader(f)
+#                 list_dict = list(my_dict)
+
+#             with open('data.json', 'w') as file:
+#                 json.dump(list_dict, file, indent=4)
+#             print("Conversion successful!")
+#             return True
+#         else:
+#             print("No CSV file provided.")
+#             return False
+#     except FileNotFoundError:
+#         print(f"File not found: {csv_file}")
+#         return False
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+#         return False
