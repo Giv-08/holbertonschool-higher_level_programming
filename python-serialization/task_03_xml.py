b0d4c66d042d7b61fa81209a9792266b9c51e2f7
chutima puthachon <chutima.p8@gmail.com>
2024-10-03 12:29:44 +1000

    added task_03_xml.py

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,28 +1,6 @@
-#!/usr/bin/python3
-"""This module demonstrates serialization and
-deserialization using XML as an alternative format to JSON
-"""
 import xml.etree.ElementTree as ET
-from xml.dom import minidom
 import xml.dom.minidom
 
-
-# def serialize_to_xml(dictionary, filename):
-#     root = ET.Element("data")
-#     for key, value in dictionary.items():
-#         child = ET.SubElement(root, key)
-#         child.text = str(value)
-
-#     # tree = ET.ElementTree(root)
-#     # xmlstr = ET.tostring(root, encoding='utf-8', method='xml')
-#     # pretty_xml = minidom.parseString(xmlstr).toprettyxml(indent="    ")
-#     # tree.write(filename, encoding='utf-8', xml_declaration=True)
-
-#     # with open(filename, 'w') as file:
-#     #     file.write(pretty_xml)
-
-#     dom = xml.dom.minidom.parse(filename) # or xml.dom.minidom.parseString(xml_string)
-#     pretty_xml_as_string = dom.toprettyxml()
 def serialize_to_xml(dictionary, filename):
     root = ET.Element("data")
     for key, value in dictionary.items():
