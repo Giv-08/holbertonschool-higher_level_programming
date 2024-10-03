150657f2150c9516fb78226939b64e32dad2ffa3
chutima puthachon <chutima.p8@gmail.com>
2024-10-03 12:22:01 +1000

    added task_03_xml.py

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -7,22 +7,36 @@ from xml.dom import minidom
 import xml.dom.minidom
 
 
+# def serialize_to_xml(dictionary, filename):
+#     root = ET.Element("data")
+#     for key, value in dictionary.items():
+#         child = ET.SubElement(root, key)
+#         child.text = str(value)
+
+#     # tree = ET.ElementTree(root)
+#     # xmlstr = ET.tostring(root, encoding='utf-8', method='xml')
+#     # pretty_xml = minidom.parseString(xmlstr).toprettyxml(indent="    ")
+#     # tree.write(filename, encoding='utf-8', xml_declaration=True)
+
+#     # with open(filename, 'w') as file:
+#     #     file.write(pretty_xml)
+
+#     dom = xml.dom.minidom.parse(filename) # or xml.dom.minidom.parseString(xml_string)
+#     pretty_xml_as_string = dom.toprettyxml()
 def serialize_to_xml(dictionary, filename):
     root = ET.Element("data")
     for key, value in dictionary.items():
         child = ET.SubElement(root, key)
         child.text = str(value)
 
-    # tree = ET.ElementTree(root)
-    # xmlstr = ET.tostring(root, encoding='utf-8', method='xml')
-    # pretty_xml = minidom.parseString(xmlstr).toprettyxml(indent="    ")
-    # tree.write(filename, encoding='utf-8', xml_declaration=True)
     xmlstr = ET.tostring(root, encoding='utf-8', method='xml').decode()
 
-    dom = xml.dom.minidom.parse(xmlstr)
+    dom = xml.dom.minidom.parseString(xmlstr)
     pretty_xml = dom.toprettyxml(indent="    ")
+
     with open(filename, 'w') as file:
         file.write(pretty_xml)
+
 def convert_type(value):
     try:
         return int(value)
