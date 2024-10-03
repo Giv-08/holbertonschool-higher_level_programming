bd9bb3f7b26f589a1e645a0ba400745b2d5596bf
chutima puthachon <chutima.p8@gmail.com>
2024-10-03 12:41:19 +1000

    added task_03_xml.py

--- python-serialization/3-main.py
+++ python-serialization/3-main.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
-from task_03_xml import serialize_to_xml, deserialize_from_xml
+#!/usr/bin/python3
+
+xml_serialize = __import__('task_03_xml')
 
 def main():
     sample_dict = {
@@ -9,10 +10,10 @@ def main():
     }
 
     xml_file = "data.xml"
-    serialize_to_xml(sample_dict, xml_file)
+    xml_serialize.serialize_to_xml(sample_dict, xml_file)
     print(f"Dictionary serialized to {xml_file}")
 
-    deserialized_data = deserialize_from_xml(xml_file)
+    deserialized_data = xml_serialize.deserialize_from_xml(xml_file)
     print("\nDeserialized Data:")
     print(deserialized_data)
 
