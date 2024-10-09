646501e6bc4f2302f4f9059b540db766e2b84608
chutima puthachon <chutima.p8@gmail.com>
2024-10-09 15:41:56 +1100

    added task_03_http_server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -29,7 +29,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'OK')
         else:
-            self.send_error(404)
+            self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'Endpoint not found')
