6fa9bc16af67d3886034c15b66fbd58d5cd7c11c
chutima puthachon <chutima.p8@gmail.com>
2024-10-09 11:27:05 +1100

    added task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,9 +7,20 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, this is a simple API!')
+        if self.path == '/data':
+            res_data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        else:
+            self.send_error(404)
+            self.end_headers()
+            self.wfile.write(b'Endpoint not found')
 
 httpd = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 httpd.serve_forever()
