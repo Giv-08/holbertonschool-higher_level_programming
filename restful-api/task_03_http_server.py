71b8c8f7b0ff9da919e355bf739e451af74929a1
chutima puthachon <chutima.p8@gmail.com>
2024-10-09 11:44:03 +1100

    added task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -34,5 +34,10 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'404 Not Found: Endpoint not found')
 
-httpd = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-httpd.serve_forever()
+# httpd = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+# httpd.serve_forever()
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print("Server started on port 8000...")
+    httpd.serve_forever()
