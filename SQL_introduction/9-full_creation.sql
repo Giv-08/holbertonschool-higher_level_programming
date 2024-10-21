fdde5a8f467317b5e802b0f8e02e19dd25529039
chutima puthachon <chutima.p8@gmail.com>
2024-10-21 19:32:22 +1100

    added 9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- Creates a table second_table in the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table (
+CREATE TABLE second_table (
   id INT,
   name VARCHAR(256),
   score INT
 );
 
-INSERT OR IGNORE INTO second_table (id, name, score)
+INSERT IGNORE INTO second_table (id, name, score)
 VALUES (1, 'John', 10),
       (2, 'Alex', 3),
       (3, 'Bob', 14),
