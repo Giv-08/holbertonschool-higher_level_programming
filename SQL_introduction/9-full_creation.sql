546ded4e03b8e53bbeb788e0bacd4734eecb652b
chutima puthachon <chutima.p8@gmail.com>
2024-10-21 19:29:41 +1100

    added 9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE second_table (
   score INT
 );
 
-INSERT INTO second_table (id, name, score)
+INSERT OR IGNORE INTO second_table (id, name, score)
 VALUES (1, 'John', 10),
       (2, 'Alex', 3),
       (3, 'Bob', 14),
