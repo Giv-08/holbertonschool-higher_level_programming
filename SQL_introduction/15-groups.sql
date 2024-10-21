27028f3c262425b1760c08db61b610b3b4a2847f
chutima puthachon <chutima.p8@gmail.com>
2024-10-21 22:05:28 +1100

    added 15-group.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Lists the number of records with the same score in the table second_table
-SELECT COUNT(*) AS number
+SELECT COUNT(*) AS score, number
 FROM second_table GROUP BY score
 ORDER BY score DESC;
