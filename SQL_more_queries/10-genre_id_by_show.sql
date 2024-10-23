5c12271dc472e35408d387694a941f1986f6d70b
chutima puthachon <chutima.p8@gmail.com>
2024-10-23 21:17:41 +1100

    added 9-cities_by_state_join

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 USE hbtn_0d_tvshows;
 SELECT * FROM
-SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
+SELECT cities.id, cities.name, states.name
+WHERE states.id = cities.state_id
 FROM cities
 ORDER BY cities.id ASC;
