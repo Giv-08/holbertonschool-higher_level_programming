a7a2bbb3b938fae125350a65e580e0010c5271da
chutima puthachon <chutima.p8@gmail.com>
2024-10-23 20:46:24 +1100

    added 8-cities_of_california_subquery

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa
 USE hbtn_0d_usa;
-SELECT name FROM cities
+SELECT id, name FROM cities
 WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
