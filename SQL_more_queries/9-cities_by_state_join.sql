a76358c74591d1aa0e07ab00ade57cb1c928d6c1
chutima puthachon <chutima.p8@gmail.com>
2024-10-24 07:22:29 +1100

    fixed 9-cities_by_state_join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa
-USE hbtn_0d_usa;
+-- USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
