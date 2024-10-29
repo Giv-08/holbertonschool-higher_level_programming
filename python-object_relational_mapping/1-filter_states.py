59af279761112328f78de6f2e09b7ec5ad6f39be
chutima puthachon <chutima.p8@gmail.com>
2024-10-29 20:20:14 +1100

    added 1-filter_states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This module is for filtering states starts witn 'n' using MySQLdb"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC LIMIT 2")
+    n_states = cursor.fetchall()
+
+    for n in n_states:
+        print(n)
+
+    db_connection.close()
