46ead0e550b161a8d4d2ab3fa721b6ef997add5a
chutima puthachon <chutima.p8@gmail.com>
2024-10-29 19:53:47 +1100

    added 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    db_connection.close()
