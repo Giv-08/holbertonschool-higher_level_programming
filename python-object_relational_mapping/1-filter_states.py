2d3ecbb3a5bb03e57cb800b19d0582e5505d2ac8
chutima puthachon <chutima.p8@gmail.com>
2024-10-29 20:23:29 +1100

    added 1-filter_states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         )
 
     cursor = db_connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC LIMIT 2")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     n_states = cursor.fetchall()
 
     for n in n_states:
