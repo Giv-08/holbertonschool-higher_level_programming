1936cf0af5c588ab393ba61b36a1771da78a8fc7
chutima puthachon <chutima.p8@gmail.com>
2024-11-01 15:20:30 +1100

    added 14-model_city_fetch

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """ This module is a state model class"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-import sys
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -14,3 +14,5 @@ class State(Base):
     id = Column(Integer, primary_key=True, unique=True,
                 autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    cities = relationship('City', back_populates='state')
